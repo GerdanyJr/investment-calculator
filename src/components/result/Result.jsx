a6228c1039dbd2ca80b77549fb439e7509301b15
Gerdany Junior <Asa_lindo@hotmail.com>
2023-08-02 10:37:40 -0300

    refactor(component) add calculation logic

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,28 +1,44 @@
 import ResultRow from "./ResultRow";
+const formatter =  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+})
 
 const Result = (props) => {
-    <table className="result">
-        <thead>
-            <tr>
-                <th>Year</th>
-                <th>Total Savings</th>
-                <th>Interest (Year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
-            </tr>
-        </thead>
-        <tbody>
-            {props.results.map(result => {
-                <ResultRow
-                    yearNumber={result.yearNumber}
-                    saving={result.saving}
-                    interest={result.interest}
-                    gainedInterest={result.gainedInterest}
-                    totalInvested={result.totalInvested}
-                />
-            })}
-        </tbody>
-    </table>
+    return (
+        <table className="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Total Savings</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.results.map(result => {
+                    return (
+                        <ResultRow
+                            key={result.year}
+                            yearNumber={result.year}
+                            saving={formatter.format(result.savingsEndOfYear)}
+                            interest={formatter.format(result.yearlyInterest)}
+                            totalInterest={formatter.format(result.savingsEndOfYear -
+                                props.initialInvestment -
+                                result.yearlyContribution * result.year)
+                            }
+                            investedCapital={formatter.format(props.initialInvestment +
+                                result.yearlyContribution * result.year)
+                            }
+                        />
+                    )
+                })}
+            </tbody>
+        </table>
+    )
 }
 
 export default Result;
--- src/components/result/ResultRow.jsx
+++ src/components/result/ResultRow.jsx
@@ -1,11 +1,13 @@
 const ResultRow = (props) => {
-    <tr>
-        <td>{props.yearNumber}</td>
-        <td>{props.saving}</td>
-        <td>{props.interest}</td>
-        <td>{props.gainedInterest}</td>
-        <td>{props.totalInvested}</td>
-    </tr>
+    return (
+        <tr>
+            <td>{props.yearNumber}</td>
+            <td>{props.saving}</td>
+            <td>{props.interest}</td>
+            <td>{props.totalInterest}</td>
+            <td>{props.investedCapital}</td>
+        </tr>
+    )
 }
 
 export default ResultRow;
